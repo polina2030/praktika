f6577d9e6145c495c17c3cad0c84cf9b8ee317dd
polina2030 <136896266+polina2030@users.noreply.github.com>
2023-06-20 19:14:18 +0700

    Update footer.jsx

--- src/pages/footer/footer.jsx
+++ src/pages/footer/footer.jsx
@@ -12,7 +12,7 @@ const PageFooter = () => {
                         <p className="ppp_footer">Кофейня Hot Coffee</p>
                         <p className="ppp_footer">г. Барнаул, ул. Ленина, 55</p>
                     </div>
-                    <div className = "networks">
+                    <div className = "networks_footer">
                         <a href=""><img src="images/icons/вк.png" alt="" className = "img_nav" /></a>
                         <a href=""><img src="images/icons/инстаграм.png" alt="" className = "img_nav" /></a>
                         <a href=""><img src="images/icons/фейсбук.png" alt="" className = "img_nav" /></a>
